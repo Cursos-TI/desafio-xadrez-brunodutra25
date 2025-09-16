e1fe11ec65113a5b890ef0a9944c573544493836
Bruno Dutra Veleis <brunodutra2@hotmail.com>
2025-09-16 04:14:57 +0000

    Adicionado as funções recursivas

--- xadrez.c
+++ xadrez.c
@@ -2,40 +2,68 @@
 
 #include <stdio.h>
 
+//Função para movimentação do Bispo usando a recursividade e loops aninhados
+void moverbispo(int casas) {
+    if (casas > 0) {
+        int vertical = 0;
+        while (vertical >= 0) {
+            printf("Cima ");            //Loop externo imprime o movimento vertical
+            vertical--;
+            for (int horizontal = 0; horizontal >= 0; horizontal--) {
+                printf("Direita");      //Loop interno imprime o movimento horizontal
+            }
+            printf("\n");               //Impressão para pular linha para melhor organização do código
+        }
+        moverbispo(casas - 1);
+    }
+}
+
+//Função para movimentação da Torre usando a recursividade
+void movertorre(int casas) {
+    if (casas > 0) {
+        printf("Direita\n");
+        movertorre(casas - 1);
+    }
+}
+
+//Função para movimentação da Rainha usando a recursividade
+void moverrainha(int casas) {
+    if (casas > 0) {
+        printf("Esquerda\n");
+        moverrainha(casas - 1);
+    }
+}
+
+//Função para movimentação do cavalo usando loops múltiplos
+void movercavalo(int casas) {
+    if (casas > 0) {
+        for (int v=2, h=1; v<=3 && h>=0; v++, h--) {
+            printf("Cima\n");
+        }
+        printf("Direita\n");
+    }
+}
+
 int main() {
-    //Declaração das variáveis do código
+    //Declaração das variáveis com a quantidade de movimentos de cada peça
     int bispo=5, torre=5, rainha=8, cavalo=2;
 
-    //Bloco de código para a movimentação do bispo 5 casas na diagonal usando a estrutura de repetição "while"
+    //Bloco de código para a movimentação do bispo usando a função "moverbispo"
     printf("Movimentação do bispo: \n");
-    while (bispo <= 9) {
-        printf("Cima direita\n");       //Imprime a direção do movimento
-        bispo++;
-    }
+    moverbispo(bispo);
 
-    //Bloco de código para a movimentação da torre 5 casas para cima usando a estrutura de repetição "do-while"
+    //Bloco de código para a movimentação da torre usando a função "movertorre"
     printf("\nMovimentação da torre: \n");
-    do {
-        printf("Cima\n");             //Imprime a direção do movimento
-        torre++;
-    } while (torre <= 9);
+    movertorre(torre);
 
 
-    //Bloco de código para a movimentação da rainha 8 casas para a direita usando a estrutura de repetição "for"
+    //Bloco de código para a movimentação da rainha usando a função "moverrainha"
     printf("\nMovimentação da rainha: \n");
-    for (int i=0; rainha<=15; rainha++) {
-        printf("Direita\n");            //Imprime a direção do movimento
-    }
+    moverrainha(rainha);
 
-    //Bloco de código para a movimentação do cavalo 2 casas para cime e uma para a direita usando loops aninhados
+    //Bloco de código para a movimentação do cavalo 2 casas para cime e uma para a direita usando loops múltiplos
     printf("\nMovimentação do cavalo: \n");
-    while (cavalo >= 2){
-        for (int i=1; i<=cavalo; i++) {
-            printf("Cima\n");           //Imprime a direção do movimento do loop interno
-        }
-        printf("Direita\n");            //Imprime a direção do movimento do loop externo
-        cavalo--;
-    }
+    movercavalo(cavalo);
 
     return 0;
 }
